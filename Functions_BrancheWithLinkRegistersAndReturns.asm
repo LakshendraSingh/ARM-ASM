1596464315bad2190f22dc513b7d92da07cdbb1b
Cyberdyne Systems Model 101 (T-800) <51139116+LakshendraSingh@users.noreply.github.com>
2024-10-06 22:54:55 +0530

    Add files via upload

---
+++ Functions_BrancheWithLinkRegistersAndReturns.asm
@@ -0,0 +1,10 @@
+.global _start
+_start:
+	MOV R0, #1
+	MOV R1, #3
+	BL add2 @BL -> Branch Linked, Uses Register and stores itself the location of the code that follows this branch
+	MOV R3, #4
+
+add2:
+	ADD R2, R0, R1
+	bx lr @branch back to location stored in register
